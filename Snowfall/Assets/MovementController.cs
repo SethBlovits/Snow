5d3c5d2f9367837a074f48827412cfbb4234f945
SethBlovits <sethblovits@gmail.com>
2023-12-26 14:19:58 -0500

    Fixed Jumping
    Jumping works now

--- Snowfall/Assets/MovementController.cs
+++ Snowfall/Assets/MovementController.cs
@@ -10,6 +10,7 @@ public class MovementController : MonoBehaviour
     Rigidbody m_rigidbody;
     public float gravity;
     bool jumpBuffered = false;
+    bool canJump = true; 
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
@@ -27,23 +28,34 @@ public class MovementController : MonoBehaviour
         if(Input.GetKey("s")){
             transform.position += -transform.forward*Time.deltaTime*speed;
         }
-        if(Input.GetKeyDown("space")){
+        if(Input.GetKeyDown("space") && canJump){
             jumpBuffered = true;
         }
     }
     // Update is called once per frame
+
     void Update() {
         
         transform.eulerAngles = new Vector3(0,transform.rotation.eulerAngles.y,0);
-        buttonWatcher();
+        buttonWatcher(); 
         
     }
     void FixedUpdate(){
-        if(jumpBuffered){
+        if(jumpBuffered && canJump){
             m_rigidbody.AddForce(transform.up*jumpheight);
             jumpBuffered=false;
         }
         m_rigidbody.AddForce(Vector3.down * gravity * m_rigidbody.mass);
         
+    }
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == "Floor"){
+            canJump = true;
+        }
+    }
+    void OnCollisionExit(Collision collision){
+        if(collision.gameObject.name == "Floor"){
+            canJump = false;
+        }
     }
 }
